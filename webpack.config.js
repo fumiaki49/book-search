0ff958e42661fc613d29af392dd575e89982a568
fumiaki49 <me.fk39@gmail.com>
2020-07-10 16:06:44 +0900

    webpackでbabelを使うための設定を記述

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mode: 'production',
+  watch: true,
+  entry: `./src/js/index.js`,
+  output: {
+    path: `${__dirname}/dist/js`,
+    filename: "main.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
